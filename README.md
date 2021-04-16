b0fe245c71816759bd615a41faab90d92bffee0a
Funnystyle <esero@hanmail.net>
2021-04-16 09:58:18 +0900

    kakao

--- README.md
+++ README.md
@@ -13,4 +13,8 @@ https://github.com/iemadk/BarCode-reader
 
 https://velog.io/@lisasu/%EC%B9%B4%EC%B9%B4%EC%98%A4-API%EC%99%80-%EB%84%A4%EC%9D%B4%EB%B2%84-API-%EC%9D%B4%EC%9A%A9
 
+https://sinna94.tistory.com/entry/%EC%B9%B4%EC%B9%B4%EC%98%A4-API-%EB%A1%9C-%EC%B1%85-%EC%A0%95%EB%B3%B4-%EA%B2%80%EC%83%89-%ED%95%98%EA%B8%B0?category=696606?category=696606
+
+https://steemit.com/kr/@anpigon/open-api
+
 sVteODrCOzSgXs9y85S9/1CpmsOUdQv
--- src/api.js
+++ src/api.js
@@ -1,18 +1,25 @@
 import axios from "axios";
 
+// const api = axios.create({
+//   baseURL: "https://openapi.naver.com/v1/search/book.json",
+//   headers: {
+//     "X-Naver-Client-Id": "sVteODrCOzSgXs9y85S9",
+//     "X-Naver-Client-Secret": "1CpmsOUdQv"
+//   },
+// });
+
 const api = axios.create({
-  baseURL: "https://openapi.naver.com/v1/search/book.json",
-  headers: {
-    "X-Naver-Client-Id": "sVteODrCOzSgXs9y85S9",
-    "X-Naver-Client-Secret": "1CpmsOUdQv"
-  },
-});
+    baseURL: "https://dapi.kakao.com/v3/search/book",
+    headers: {
+      "Authorization": "KakaoAK d45404c5fe73d1b8decee15ba1182a36",
+    },
+  });
 
 export const bookApi = {
   search: (term) =>
   api.get("", {
     params: {
-      query: encodeURIComponent(term),
+      query: term,
     },
   }),
 };
